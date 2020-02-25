1e3e196497296f1d26f431e2d1d0de27f4fa3a50
daniel-voicu <voicu_ilie_daniel@yahoo.com>
2020-02-25 15:35:26 +0200

    require user to login before accessing the website

--- app/__init__.py
+++ app/__init__.py
@@ -10,5 +10,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 
+# automatically redirect the user to the login form, and only redirect back to the page the user wanted to view
+# after the login process is complete.
+login.login_view = 'login'
+
 # this bottom impoort is a workaround to circular imports. 'routes' module needs to import the app variable defined in this script
 from app import routes, models
