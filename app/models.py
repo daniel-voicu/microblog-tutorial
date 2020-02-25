ac0ee34f75dc655d2b10625893a9b67c9127a23f
daniel-voicu <voicu_ilie_daniel@yahoo.com>
2020-02-25 15:10:51 +0200

    add a user loader function that help flask-login to load a user

--- app/models.py
+++ app/models.py
@@ -1,9 +1,25 @@
-from app import db
+from flask_login import UserMixin
+
+from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
+"""
+Because Flask-Login knows nothing about databases, it needs the application's help in loading a user.
+For that reason, the extension expects that the application will configure a user loader function,
+that can be called to load a user given the ID
+The user loader is registered with Flask-Login with the @login.user_loader decorator. 
+The id that Flask-Login passes to the function as an argument is going to be a string, so databases that 
+use numeric IDs need to convert the string to integer as you see above.
+"""
 
-class User(db.Model):
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -18,7 +34,6 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
 
 
 class Post(db.Model):
