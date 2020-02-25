7cabc0135a0768bbdadc1710b94de3aff9d2d0ea
daniel-voicu <voicu_ilie_daniel@yahoo.com>
2020-02-25 11:27:26 +0200

    create app configuration object

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # this bottom impoort is a workaround to circular imports. 'routes' module needs to import the app variable defined in this script
 from app import routes
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or b'+\xd0\x8e\x93H\x9a\xa2\x10\x10q\xe2\xff\xa8\xe4\xb2\x12\xe7\x97\x1c!\xd61=\x94'
