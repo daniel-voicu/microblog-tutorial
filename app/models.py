729181710ad5a5b52aedd4fc2fd8cd85fdf131a7
daniel-voicu <voicu_ilie_daniel@yahoo.com>
2020-02-25 16:27:50 +0200

    add user profile page

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from flask_login import UserMixin
-
+from hashlib import md5
 from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -34,6 +34,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 class Post(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -40,6 +40,18 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
