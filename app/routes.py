1f18347ef113cd931e0545b3d403f7a063b97d3c
daniel-voicu <voicu_ilie_daniel@yahoo.com>
2020-02-26 17:33:28 +0200

    add translate posts functionality. since I don't have an external translate service, I'll return the destination language + initial text value

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
 
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, PostForm, ResetPasswordForm
@@ -10,6 +10,9 @@ from app.forms import EditProfileForm
 from app.forms import ResetPasswordRequestForm
 from app.email_sender import send_password_reset_email
 from flask_babel import _, get_locale
+from guess_language import guess_language
+
+from app.translate import translate
 
 
 @app.before_request
@@ -27,7 +30,11 @@ def index():
     form = PostForm()
 
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -187,3 +194,10 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify(
+        {'text': translate(request.form['text'], request.form['source_language'], request.form['dest_language'])})
