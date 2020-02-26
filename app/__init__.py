cd1f35d3c2ef8774ac8e11f19a53b6d49dd49145
daniel-voicu <voicu_ilie_daniel@yahoo.com>
2020-02-26 13:55:56 +0200

    add option to reset password

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,9 +20,6 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
-
-# this bottom impoort is a workaround to circular imports. 'routes' module needs to import the app variable defined in this script
-from app import routes, models, errors
 
 if not app.debug:
     # configure logging to email
@@ -54,3 +51,7 @@ app.logger.addHandler(file_handler)
 
 app.logger.setLevel(logging.INFO)
 app.logger.info('Microblog startup')
+
+# this bottom impoort is a workaround to circular imports. 'routes' module needs to import the app variable defined
+# in this script
+from app import routes, models, errors
